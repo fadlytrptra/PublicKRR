96b43cb8af83c6fc5e2f77dc0444a1f475394ecd
christoferromeo0207 <christoferromeo0207@gmail.com>
2026-04-20 13:41:18 +0700

    fix<PUBLICWEB>
    - perbaikan get NPWP Customer pada  halaman profile_modal

--- resources/views/profile_modal.blade.php
+++ resources/views/profile_modal.blade.php
@@ -1,33 +1,28 @@
-
-<!--Hanya NPWP-->
 @php
-$npwpCustomers = DB::connection('ConnPublic')
-    ->table('CustomerUserPublic')
-    ->where('IdUser', session('user')->IdUser)
-    ->whereNotNull('NPWP')
-    ->pluck('NPWP')
-    ->unique()
-    ->values();
+$user = session('user');
+
+$npwpCustomers = collect();
+
+if ($user) {
+    $npwpCustomers = DB::connection('ConnPublic')
+        ->table('CustomerUserPublic')
+        ->where('IdUser', $user->IdUser)
+        ->whereNotNull('NPWP')
+        ->pluck('NPWP')
+        ->unique()
+        ->values();
+}
 @endphp
-
-<!--Dengan ID Cust-->
-{{-- @php
-$npwpCustomers = DB::connection('ConnPublic')
-    ->table('CustomerUserPublic')
-    ->where('IdUser', session('user')->IdUser)
-    ->whereNotNull('NPWP')
-    ->select('IDCust', 'NPWP')
-    ->get();
-@endphp --}}
-
 
 
 <div class="modal fade" id="profileModal" tabindex="-1">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
 
-            @if(session('user'))
-                <form action="{{ route('profile.update', session('user')->IdUser) }}" method="POST" novalidate>
+            @php $user = session('user'); @endphp
+
+                @if($user)
+                <form action="{{ route('profile.update', $user->IdUser) }}">
                     @csrf
                     @method('PUT')
 
